9cb4089c38f134b5efece53bd4e71b9793717922
ARKA99 <155990363+Arkady465@users.noreply.github.com>
2025-11-28 14:10:29 +0300

    Add files via upload

---
+++ main-service/src/main/java/ru/practicum/main_service/event/model/Event.java
@@ -0,0 +1,70 @@
+package ru.practicum.main_service.event.model;
+
+import lombok.*;
+import ru.practicum.main_service.category.model.Category;
+import ru.practicum.main_service.event.enums.EventState;
+import ru.practicum.main_service.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2000)
+    private String annotation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false, length = 7000)
+    private String description;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDateTime eventDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "initiator_id", nullable = false)
+    private User initiator;
+
+    @Embedded
+    private Location location;
+
+    @Column(nullable = false)
+    private Boolean paid;
+
+    @Column(name = "participant_limit")
+    private Integer participantLimit;
+
+    @Column(name = "published_on")
+    private LocalDateTime publishedOn;
+
+    @Column(name = "request_moderation")
+    private Boolean requestModeration;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private EventState state;
+
+    @Column(nullable = false, length = 120)
+    private String title;
+
+    @Transient
+    private Long views;
+
+    @Transient
+    private Long confirmedRequests;
+}
---
+++ main-service/src/main/java/ru/practicum/main_service/event/service/EventService.java
@@ -0,0 +1,31 @@
+package ru.practicum.main_service.event.service;
+
+import ru.practicum.main_service.event.dto.*;
+import ru.practicum.main_service.event.enums.EventState;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface EventService {
+    List<EventShortDto> getEventsByUser(Long userId, Integer from, Integer size);
+
+    EventFullDto createEvent(Long userId, NewEventDto newEventDto);
+
+    EventFullDto getEventByUser(Long userId, Long eventId);
+
+    EventFullDto updateEventByUser(Long userId, Long eventId, UpdateEventRequest updateEvent);
+
+    List<EventFullDto> getEventsByAdmin(List<Long> users, List<EventState> states,
+                                        List<Long> categories, LocalDateTime rangeStart,
+                                        LocalDateTime rangeEnd, Integer from, Integer size);
+
+    EventFullDto updateEventByAdmin(Long eventId, UpdateEventRequest updateEvent);
+
+    List<EventShortDto> getEventsPublic(String text, List<Long> categories, Boolean paid,
+                                        LocalDateTime rangeStart, LocalDateTime rangeEnd,
+                                        Boolean onlyAvailable, String sort,
+                                        Integer from, Integer size, HttpServletRequest request);
+
+    EventFullDto getEventPublic(Long eventId, HttpServletRequest request);
+}

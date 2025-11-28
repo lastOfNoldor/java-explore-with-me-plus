2529f764b7c19bea06f285a4a3a33eb232f3e54f
ARKA99 <155990363+Arkady465@users.noreply.github.com>
2025-11-28 15:51:24 +0300

    Delete main-service directory

--- main-service/src/main/java/ru/practicum/main_service/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.main_service.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practicum.main_service.user.model.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}

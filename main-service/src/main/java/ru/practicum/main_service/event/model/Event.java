7a7966e829d331e8e511bbefb5e11410cb07bd6d
lastOfNoldor <martynovoo95@gmail.com>
2025-12-01 16:43:30 +0300

    2ой коммит переработанной ветки events

--- main-service/src/main/java/ru/practicum/main_service/event/model/Event.java
+++ main-service/src/main/java/ru/practicum/main_service/event/model/Event.java
@@ -1,39 +1,55 @@
 package ru.practicum.main_service.event.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 import ru.practicum.main_service.category.model.Category;
+import ru.practicum.main_service.user.model.User;
 
 import java.time.LocalDateTime;
 
 @Entity
-
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, length = 120)
     private String title;
+    @Column(nullable = false, length = 2000)
     private String annotation;
+    @Column(nullable = false, length = 7000)
     private String description;
-
+    @Column(name = "created_on")
     private LocalDateTime createdOn;
+    @Column(name = "event_date", nullable = false)
     private LocalDateTime eventDate;
+    @Column(name = "published_on")
     private LocalDateTime publishedOn;
     @Enumerated(EnumType.STRING)
     private EventState state;
+    @Column(name = "request_moderation")
     private Boolean requestModeration;
-
+    @Column(name = "participant_limit")
     private Integer participantLimit;
+    @Column(nullable = false)
     private Boolean paid;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
-    private Long initiatorId;     // id вместо  объектов ?
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "initiator_id", nullable = false)
+    private User initiator;
+    @Embedded
     private Location location;
 
     // Эти поля НЕ должны быть в Entity - вычисляются
     // private Long confirmedRequests;
     // private Long views;
+    // createdOn; ставится в сервисе
+    // paid  ставится в сервисе
 }

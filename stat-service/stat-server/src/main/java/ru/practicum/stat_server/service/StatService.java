c835d0ff01ffbba7de9a890bfcda8d0a3ed0f6b9
lastOfNoldor <martynovoo95@gmail.com>
2025-12-06 01:44:05 +0300

    final last test fix 100%

--- stat-service/stat-server/src/main/java/ru/practicum/stat_server/service/StatService.java
+++ stat-service/stat-server/src/main/java/ru/practicum/stat_server/service/StatService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.stat_dto.EndpointHitDto;
 import ru.practicum.stat_dto.ViewStatsDto;
+import ru.practicum.stat_server.exception.ValidationException;
 import ru.practicum.stat_server.mapper.StatMapper;
 import ru.practicum.stat_server.model.EndpointHit;
 import ru.practicum.stat_server.repository.StatRepository;
@@ -28,6 +29,12 @@ public class StatService {
     public List<ViewStatsDto> getStats(String start, String end, List<String> uris, Boolean unique) {
         LocalDateTime startTime = LocalDateTime.parse(start, FORMATTER);
         LocalDateTime endTime = LocalDateTime.parse(end, FORMATTER);
+        if (startTime.isAfter(endTime)) {
+            throw new ValidationException("старт дата должны быть до конечной даты");
+        }
+        if (startTime.isAfter(LocalDateTime.now())) {
+            throw new ValidationException("Дата начала не может быть в будущем");
+        }
 
         if (Boolean.TRUE.equals(unique)) {
             return statRepository.getUniqueStats(startTime, endTime, uris);
